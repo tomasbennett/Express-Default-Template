8a082634f22ce4cd613af07408b88f6de77a4bb7
tomasbennett <tjs.bennett@gmail.com>
2025-10-20 15:15:16 +0100

    Adjusted for typescript in the controllers - Not possible right now for the views, will check ORM for type safety between the node application and the postgresql database

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
-const express = require("express");
+import express from "express";
 const expressLayouts = require("express-ejs-layouts");
 const app = express();
 
 const path = require("node:path");
+
+
 
 
 app.set("view engine", "ejs");
@@ -12,18 +14,9 @@ app.use(express.urlencoded({ extended: true }));
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static("public"));
 
-// app.set("layout", "layouts/mainLayout");
-
-const controllers = require("./controllers/messageController");
+app.set("layout", "layouts/mainLayout");
 
 
-
-
-
-app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).send("500: Internal Server Error");
-});
 
 
 const PORT = process.env.PORT || 3000;
